8c6a1bac4c69d9e6c70f101755ff2df58655967e
tun19502 <johnlawall24@gmail.com>
2024-09-26 12:01:57 -0400

    selecting text changes textView size

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -19,8 +19,13 @@ class MainActivity : AppCompatActivity() {
 
         // TODO (Step 2: Define lambda to modify displayTextView size)
 
+
+        val callBack = {fontSize : Float -> displayTextView.textSize = fontSize }
+
         // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
+
+
+        recyclerView.adapter = NumberDisplayAdapter(numbers, callBack)
 
 
     }
